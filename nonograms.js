8b833df8251802b6da1c4590ea37b7d2fd4c9380
kellyserafico <seraficok@gmail.com>
2023-09-25 18:31:19 -0700

    added solution

--- nonograms.js
+++ nonograms.js
@@ -1,8 +1,3 @@
-var table = document.getElementById("canvasBoard");
-createEmptyCells();
-let isMouseDown = false;
-let isRightClickDown = false;
-document.addEventListener('contextmenu', (e) => e.preventDefault());
 const solution = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,6 +10,12 @@ const solution = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
+let wrongCounter = 100;
+var table = document.getElementById("canvasBoard");
+createEmptyCells();
+let isMouseDown = false;
+let isRightClickDown = false;
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
 
 function createEmptyCells() {
     for (let i = 0; i < 11; i++) {
@@ -29,15 +30,19 @@ function createEmptyCells() {
                 cell.innerHTML = ": 3";
                 cell.classList.add("nums");
             } else {
-                createClickableButtons(cell);
+                createClickableButtons(cell, solution[i-1][j-1]);
             }
         }
     }
 }
 
-function createClickableButtons(cell) {
+function createClickableButtons(cell, correctValue) {
     const button = document.createElement('button');
-
+    button.value = correctValue;
+    if (correctValue == 0){
+        wrongCounter -= 1;
+    }
+    
     button.addEventListener('mousedown', function (event) {
         if (event.button === 0) { // Left mouse button
             isMouseDown = true;
@@ -91,23 +96,37 @@ function toggleButton(button) {
         if (cell.classList.contains('activeButton')) {  //if right clicking on activeButton
             cell.classList.remove('activeButton');
             // console.log("removed");
+            if (button.value == 0){
+                wrongCounter -= 1;
+            }else{
+                wrongCounter += 1;
+            }
         }
         else if(!(cell.classList.contains('activeButton'))){ // if right clicking on a blank button, will add xButton class (make red)
-            if(cell.classList.contains('xButton')){
-                cell.classList.remove('xButton')
-            }
-            else if(!(cell.classList.contains('xButton'))){
-                cell.classList.add('xButton')
-            }
+            // cell.classList.add('xButton')
         }
     } 
     else if (isMouseDown && !isRightClickDown ){ // Only toggle on left-click
         if(cell.classList.contains('xButton')){
             cell.classList.remove('xButton')
         }
-        cell.classList.add('activeButton');
+        if (!cell.classList.contains('activeButton')){
+            cell.classList.add('activeButton');
+            if (button.value == 1){
+                wrongCounter -= 1;
+            }else{
+                wrongCounter += 1;
+            }
+        }
         // console.log("added");
 
+    }
+    console.log(wrongCounter);
+    if (wrongCounter == 0){
+        document.getElementById('text').innerHTML = "yay"
+    }
+    else{
+        document.getElementById('text').innerHTML = "no : ("
     }
 }
 
@@ -116,31 +135,29 @@ function toggleButton(button) {
   
   console.log(solution)
 
-function checkSolution(){
-    let buttons = document.querySelectorAll('.cell button');
-    let isSolutionCorrect = true;
+// function checkSolution(){
+//     let buttons = document.querySelectorAll('.cell button');
+//     let isSolutionCorrect = true;
 
-    buttons.forEach((button, index) => {
-        const row = Math.floor(index / 10);
-        const col = index % 10;
+//     buttons.forEach((button, index) => {
+//         const row = Math.floor(index / 10);
+//         const col = index % 10;
 
-        button.addEventListener('click', () => {
-            console.log(`Button clicked at Row: ${row}, Column: ${col}`);
-        });
+//         button.addEventListener('click', () => {
+//             console.log(`Button clicked at Row: ${row}, Column: ${col}`);
+//         });
 
-        const canvasValue = button.classList.contains('activeButton') ? 1 : 0;
-        const solutionValue = solution[row][col];
+//         const canvasValue = button.classList.contains('activeButton') ? 1 : 0;
+//         const solutionValue = solution[row][col];
 
-        console.log(`Canvas Value: ${canvasValue}, Solution Value: ${solutionValue}`);
+//         console.log(`Canvas Value: ${canvasValue}, Solution Value: ${solutionValue}`);
 
 
-        if (canvasValue !== solutionValue) {
-            isSolutionCorrect = false;
+//         if (canvasValue !== solutionValue) {
+//             isSolutionCorrect = false;
             
-        }
+//         }
 
-    });
-    if (isSolutionCorrect) {
-        alert("Congratulations! You solved the puzzle.");
-    }
-}
+//     });
+    
+// }
